53cd20250561de47ef6c480efe25bf4cfa92beb3
Brian Kaloki <brianngenye@gmail.com>
2025-03-19 17:26:04 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// Wait for the DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the paragraph element with id="text"
+    const paragraph = document.getElementById("text");
+  
+    // Change the text content of the paragraph
+    paragraph.textContent = "This is really cool!";
+  });
+  
+  // This log will appear immediately when index.js is loaded, before DOMContentLoaded fires
+  console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+  
